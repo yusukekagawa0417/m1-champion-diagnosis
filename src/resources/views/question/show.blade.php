0991c4492ce6704197e353904a229d970c4a639e
fumikaobayashi <fumika.obayashi@gmail.com>
2020-12-27 18:04:42 +0900

    QUESTION大枠作成

--- src/resources/views/question/show.blade.php
+++ src/resources/views/question/show.blade.php
@@ -1,18 +1,16 @@
 @extends('layouts.app')
 @section('title', "質問：" . config('app.name'))
 @section('content')
-    <div>
-        <h1>質問</h1>
-        <div>
+    <div class="container">
+        <div class="box">  
+            <h1>QUESTION</h1>
+        </div>
+        <div class="box">
             {{ $question['content'] }}
         </div>
-        <div>
-            <a href="{{ route($question['answer']['yes']['path'], ['id' => $question['answer']['yes']['id']]) }}">
-                はい
-            </a>
-            <a href="{{ route($question['answer']['no']['path'], ['id' => $question['answer']['no']['id']]) }}">
-                いいえ
-            </a>
+        <div class="box">
+            <a href="{{ route($question['answer']['yes']['path'], ['id' => $question['answer']['yes']['id']])}}" class="btn btn-border btn-yes">はい</a>
+            <a href="{{ route($question['answer']['no']['path'], ['id' => $question['answer']['no']['id']]) }}" class="btn btn-border btn-no">いいえ</a>
         </div>
     </div>
 @endsection
